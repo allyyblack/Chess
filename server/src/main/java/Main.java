823e1ba0985771d9d17696ddffbf1758b6198861
allyyblack <allyyblack@gmail.com>
2024-10-21 20:24:11 -0600

    login

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,12 +1,15 @@
 import chess.*;
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
 import server.Server;
 import service.ChessService;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server(ChessService service);
+        DataAccess dataAccess = new MemoryDataAccess();
         int port = 8080;
-        server.run(port);
+        var service = new ChessService(dataAccess);
+        var server = new Server(service).run(port);
 
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
--- server/src/main/java/service/ChessService.java
+++ server/src/main/java/service/ChessService.java
@@ -21,13 +21,4 @@ public class ChessService {
         String authToken = dataAccess.createAuth();
         return authToken;
     }
-
-
-
-    public Pet addPet(Pet pet) throws ResponseException {
-        if (pet.type() == PetType.DOG && pet.name().equals("fleas")) {
-            throw new ResponseException(200, "no dogs with fleas");
-        }
-        return dataAccess.addPet(pet);
-    }
 }
