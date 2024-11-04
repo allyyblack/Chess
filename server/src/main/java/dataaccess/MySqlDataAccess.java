f700f89facb4ea0f9cc9535b7726df4515e39efe
allyyblack <allyyblack@gmail.com>
2024-11-03 19:57:55 -0700

    all test cases pass

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -6,10 +6,8 @@ import model.GameData;
 import model.UserData;
 import org.mindrot.jbcrypt.BCrypt;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.sql.*;
-import java.util.UUID;
 
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static java.sql.Types.NULL;
@@ -53,22 +51,78 @@ public class MySqlDataAccess implements DataAccess {
     }
 
     public GameData createGame(String gameName) throws DataAccessException {
-        var statement = "INSERT INTO games (gameName, whiteUsername, blackUsername) VALUES (?, ?, ?)";
-        int gameID = executeUpdate(statement, gameName, null, null);
+        var statement = "INSERT INTO games (gameName, whiteUsername, blackUsername, game) VALUES (?, ?, ?, ?)";
         ChessGame game = new ChessGame();
+        var serializer = new Gson();
+        var json = serializer.toJson(game);
+        int gameID = executeUpdate(statement, gameName, null, null, json);
         return new GameData(gameID, null, null, gameName, game);
         }
 
 
         public GameData getGame(int gameID) throws DataAccessException {
-        return null;
-    }
+            try (var conn = DatabaseManager.getConnection()) {
+                var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM games WHERE gameID=?";
+                try (var ps = conn.prepareStatement(statement)) {
+                    ps.setInt(1, gameID);
+                    try (var rs = ps.executeQuery()) {
+                        if (rs.next()) {
+                            return readGame(rs);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+            }
+            return null;
+        }
 
     public Collection<GameData> listGames() throws DataAccessException {
-        return List.of();
+        List<GameData> gamesList = new ArrayList<>();
+        String statement = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM games";
+
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        int gameID = rs.getInt("gameID");
+                        String whiteUsername = rs.getString("whiteUsername");
+                        String blackUsername = rs.getString("blackUsername");
+                        String gameName = rs.getString("gameName");
+                        String gameJson = rs.getString("game");
+                        var serializer = new Gson();
+                        ChessGame chessGame = serializer.fromJson(gameJson, ChessGame.class);
+                        gamesList.add(new GameData(gameID, whiteUsername, blackUsername, gameName, chessGame));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to list games: " + e.getMessage());
+        }
+
+        return gamesList;
     }
 
     public void updateGame(int gameID, String authToken, String color) throws DataAccessException {
+//        GameData gameData = getGame(gameID);
+        if (authToken == null) {
+            throw new DataAccessException("authtoken null");
+        }
+        AuthData authData = getAuth(authToken);
+        if (authData != null) {
+            if (Objects.equals(color, "WHITE")) {
+//                GameData updatedGame = new GameData(gameID, authData.username(), gameData.blackUsername(), gameData.gameName(), gameData.game());
+                String statement = "UPDATE games SET whiteUsername = ? WHERE gameID = ?";
+                executeUpdate(statement, authData.username(), gameID);
+            }
+            if (Objects.equals(color, "BLACK")) {
+//                GameData updatedGame = new GameData(gameID, gameData.whiteUsername(), authData.username(), gameData.gameName(), gameData.game());
+                String statement = "UPDATE games SET blackUsername = ? WHERE gameID = ?";
+                executeUpdate(statement, authData.username(), gameID);
+            }
+        }
+
+
 
     }
 
@@ -81,17 +135,43 @@ public class MySqlDataAccess implements DataAccess {
     }
 
     public AuthData getAuth(String authToken) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            String statement = "SELECT auth_token, username FROM tokens WHERE auth_token = ?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String username = rs.getString("username");
+                        return new AuthData(authToken, username);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to read auth data", e.getMessage()));
+        }
         return null;
     }
 
     public void deleteAuth(AuthData authToken) throws DataAccessException {
-
+        String statement = "DELETE FROM tokens WHERE auth_token = ?";
+        executeUpdate(statement, authToken.authToken());
     }
+
     private UserData readUser(ResultSet rs) throws SQLException {
         var username = rs.getString("username");
         var passwordHash = rs.getString("password_hash");
         var email = rs.getString("email");
         return new UserData(username, passwordHash, email);
+    }
+    private GameData readGame(ResultSet rs) throws SQLException {
+        var gameID = rs.getInt("gameID");
+        var whiteUsername = rs.getString("whiteUsername");
+        var blackUsername = rs.getString("blackUsername");
+        var gameName = rs.getString("gameName");
+        var gameJson = rs.getString("game");
+        var serializer = new Gson();
+        ChessGame chessGame = serializer.fromJson(gameJson, ChessGame.class);
+        return new GameData(gameID,whiteUsername, blackUsername, gameName,chessGame);
     }
 
 
@@ -137,16 +217,16 @@ public class MySqlDataAccess implements DataAccess {
             """,
             """
             CREATE TABLE IF NOT EXISTS games (
-                `gameID` INT NOT NULL AUTO_INCREMENT,
-                `whiteUsername` VARCHAR(50) NOT NULL,
-                `blackUsername` VARCHAR(50) NOT NULL,
-                 `gameName` VARCHAR(100) NOT NULL,
-                `game` TEXT NOT NULL, -- This can hold the serialized ChessGame data
-                 PRIMARY KEY (`gameID`),
-                FOREIGN KEY (`whiteUsername`) REFERENCES users(`username`) ON DELETE CASCADE,
-                 FOREIGN KEY (`blackUsername`) REFERENCES users(`username`) ON DELETE CASCADE
-                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """,
+            `gameID` INT NOT NULL AUTO_INCREMENT,
+            `whiteUsername` VARCHAR(50) DEFAULT NULL,
+            `blackUsername` VARCHAR(50) DEFAULT NULL,
+            `gameName` VARCHAR(100) NOT NULL,
+            `game` TEXT NOT NULL, -- This can hold the serialized ChessGame data
+             PRIMARY KEY (`gameID`),
+            FOREIGN KEY (`whiteUsername`) REFERENCES users(`username`) ON DELETE SET NULL,
+            FOREIGN KEY (`blackUsername`) REFERENCES users(`username`) ON DELETE SET NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            """
     };
 
     private void configureDatabase() {
