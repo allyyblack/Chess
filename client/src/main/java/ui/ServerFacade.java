8ad9763d37f6901800ea2ab19b47555ccaa52a21
allyyblack <allyyblack@gmail.com>
2024-11-17 15:50:30 -0700

    serverfacade

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,105 @@
+package ui;
+import com.google.gson.Gson;
+import model.GameData;
+import model.PlayerGame;
+import model.UserData;
+import java.io.*;
+import java.net.*;
+import java.util.Collection;
+
+
+public class ServerFacade {
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public UserData register(UserData userData) throws ResponseException {
+        var path = "/user";
+        return this.makeRequest("POST", path, userData, UserData.class);
+    }
+
+    public UserData login(UserData userData) throws ResponseException {
+        var path = "/session";
+        return this.makeRequest("POST", path, userData, UserData.class);
+    }
+
+    public void logout(String authToken) throws ResponseException {
+        var path = String.format("/session/%s", authToken);
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+    public GameData createGame(GameData gameData, String authToken) throws ResponseException {
+        var path = String.format("/game/%s", authToken);
+        return this.makeRequest("POST", path, gameData, GameData.class);
+    }
+
+    public Collection<GameData> listGames(String authToken) throws ResponseException {
+        var path = String.format("/game/%s", authToken);
+        record listGameResponse(Collection<GameData> games) {
+        }
+        var response = this.makeRequest("GET", path, null, listGameResponse.class);
+        return response.games();
+    }
+
+    public void clearApplication() throws ResponseException {
+        var path = "/db";
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+    public PlayerGame joinGame(PlayerGame playerGame, String authToken) throws ResponseException {
+        var path = String.format("/game/%s", authToken);
+        return this.makeRequest("POST", path, playerGame, PlayerGame.class);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
