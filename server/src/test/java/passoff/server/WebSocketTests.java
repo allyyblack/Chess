f12028a1092238ee2d9eb19518c9e28afe7c37d2
allyyblack <allyyblack@gmail.com>
2024-12-09 17:34:09 -0700

    i hate this class

--- server/src/test/java/passoff/server/WebSocketTests.java
+++ server/src/test/java/passoff/server/WebSocketTests.java
@@ -13,7 +13,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static websocket.messages.ServerMessage.ServerMessageType.*;
-
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class WebSocketTests {
     private static WebsocketTestingEnvironment environment;
